60c597c6b35da32408756e05229f0949188bea69
Valerie <valeriec@san.rr.com>
2015-03-02 16:20:09 -0800

    final version

--- wp-content/themes/portfolio-theme/index.php
+++ wp-content/themes/portfolio-theme/index.php
@@ -15,6 +15,7 @@
 		<?php while( have_posts() ): the_post(); ?>
 			<section <?php post_class(); ?>>
 			<?php the_post_thumbnail('medium'); ?>
+			<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
 				<p><?php the_content(); ?> </p>
 			</section>
 
